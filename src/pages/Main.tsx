75174d61614ca2a2ff57dbef566883c3a8613ca1
Fabricio <fabricio.riff2@gmail.com>
2020-06-17 23:55:04 -0300

    implementação parcial da lógica de contagem regressiva do app

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,37 +1,90 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import moment from 'moment';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Vibration } from 'react-native';
 
 import styles from './styles';
 
-const Main: React.FC = () => {    
-    const [duration, setDuration] = useState<moment.Duration>();
-   
-    useEffect(() => {
-        const timeInitial = moment();
-        let minutes = 25;
-        let seconds = 0;
-        let hours = 0;
-        timeInitial.add(seconds, 'seconds').add(minutes, 'minutes').add(hours, 'hours');
+interface IClock {
+    minutes: number,
+    seconds: number
+}
 
-        const currentTime = moment();
+const Main: React.FC = () => {
+    var newClock: IClock = {
+        minutes: 25,
+        seconds: 0
+    }
+    const [clock, setClock] = useState<IClock>({ minutes: 2, seconds: 0 });
 
-        let diffTime = timeInitial.unix() - currentTime.unix();
-        setDuration(moment.duration(diffTime * 1000, 'milliseconds'));
-        alert(duration?.asSeconds());        
-    }, [])
+
+    const start = () => {
+
+        let seconds = 60;
+        let minutes = 2;
+        const timer = setInterval(() => {
+
+           // console.log(seconds);
+            // setClock({minutes: 25, seconds});
+            if (clock.minutes == 0 && clock.seconds === 0) {
+                clearTimeout(timer);
+            }
+
+            if (seconds > 0) {
+                seconds = seconds - 1;
+                minutes = clock.minutes - 1;
+
+                setClock({ minutes, seconds });
+            }
+            else {
+                seconds = 60;
+                seconds = seconds - 1;
+                minutes = clock.minutes--;
+
+                setClock({ minutes, seconds });
+            }
+        }, 100);
+        //   setClock({ minutes: 25, seconds: 0 });
+
+
+
+        //  console.log(clock)
+        // if (clock) {
+
+        //     setInterval(() => {
+        //         if (clock.minutes > 0 || clock.seconds > 0) {
+        //             //     alert('primeiro if' + clock.seconds);
+        //             if (clock.minutes === 0 && clock.seconds === 0) {
+        //                 clearTimeout();
+        //                 alert('teste else if' + clock.seconds);
+        //                 Vibration.vibrate([100, 200, 300, 30000], true);
+        //                 return null;
+        //             }
+
+
+        //             else {
+        //                 let minutes = clock.minutes;
+        //                 let seconds = clock.seconds - 1;
+        //                 setClock({minutes, seconds});
+        //                 // alert(clock.minutes);     
+        //                 //return null;
+        //             }
+        //         }
+        //     }, 2000);
+        // }
+    }
+
+
 
     return (
         <View>
             <View style={styles.container}>
-                <Text style={styles.text}>{duration?.asMinutes()}</Text>
+                <Text style={styles.text}>{clock?.minutes}</Text>
                 <Text style={styles.dotText}>:</Text>
-                <Text style={styles.text}>00</Text>
+                <Text style={styles.text}>{clock?.seconds}</Text>
             </View>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity onPress={start} style={styles.button}>
                 <Text style={styles.buttonText}>Iniciar</Text>
             </TouchableOpacity>
-        </View> 
+        </View>
     )
 }
 
